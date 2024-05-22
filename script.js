8baa847a8e9d35150f22b18ef9f60015fecdc820
harshveer7828 <harshveer7828@gmail.com>
2024-05-22 14:22:06 +0530

    Your commit message

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-document.querySelector('#play').addEventListener('click', function(){
-    document.querySelector('#video').play()
-    document.querySelector('#play').classList.add('hidden')
+gsap.set('.btnforvideo',{opacity:0,y:10})
 
+setInterval(function(){
+    gsap.to('.load',{
+        y:-100,
+        opacity:0,
+        ease:"power1.out"
+    },"same")
+    gsap.to('.btnforvideo',{
+        y:0,
+        delay:0.2,
+        opacity:1,
+        ease:"power2.in"
+    })
+    
+
+},6000)
+
+document.querySelector('.btnforvideo').addEventListener('click', function(){
+    gsap.to('.loader',{
+        opacity:0,
+        ease:"power2.out"
+    })
+    document.querySelector('#video').play()
     setInterval(function(){
         document.querySelector('#video').pause()
 
-    },13000)
+    },11000)
 })
